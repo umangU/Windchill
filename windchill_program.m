4d9194c264fc295a38e9c1da62c348b1166080fc
umangU <72771903+umangU@users.noreply.github.com>
2021-04-25 01:58:25 +1000

    Code Updated

--- windchill_program.m
+++ windchill_program.m
@@ -1,6 +1,9 @@
+% Getting User Inputs
 T = input("Enter the ambient air temperature in degree Celsius: ");
 v = input("Enter the windspeed in km/h: ");
+
 % Windchill function called
 p = windchill(T,v);
+
 % Displaying Output
 fprintf('If the ambient air temperature is %d degree celsius and the wind speed is %dkm/h, \nthen the apparent Windchill Temperature will be: %d \n',T,v,p);
