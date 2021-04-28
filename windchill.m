ab8221a6bd738b8f2ba89778fc4a6da261e95fb4
umangU <72771903+umangU@users.noreply.github.com>
2021-04-29 07:24:00 +1000

    Code Updated

--- windchill.m
+++ windchill.m
@@ -1,4 +1,4 @@
-% function to calculate the windchill temperature from the ambient air temperature and wind speed passed an arguments.
+% Function to calculate the windchill temperature from the ambient air temperature and wind speed passed an arguments.
 function p = windchill(T, v)
 % Windchill temperature is stored in the variable p after the calculation
 p = 13.112 + (0.6215.* T) - (11.37.* v.^ 0.16) + (0.3965.* T.* v.^0.16); 
